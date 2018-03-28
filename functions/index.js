726667d5c44015eac9e2068dc5fe6776df699330
lusan <daslusan@gmail.com>
2018-03-28 16:31:27 +0530

    imported cloud functions and firebase admin modules

--- functions/index.js
+++ functions/index.js
@@ -16,7 +16,11 @@
 
 // Note: You will edit this file in the follow up codelab about the Cloud Functions for Firebase.
 
-// TODO(DEVELOPER): Import the Cloud Functions for Firebase and the Firebase Admin modules here.
+// Import the Firebase SDK for Google Cloud Functions.
+const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // TODO(DEVELOPER): Write the addWelcomeMessages Function here.
 
